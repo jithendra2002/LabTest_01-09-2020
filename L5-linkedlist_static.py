321c01f4b6294b52ecb70e0e04e0bd0360ba97fa
jithendra2002 <70188585+jithendra2002@users.noreply.github.com>
2020-10-04 12:51:31 +0530

    Add files via upload

---
+++ L5-linkedlist_static.py
@@ -0,0 +1,26 @@
+print("Jithendra Naidu-121910313053")
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+
+
+    def printList(self):
+        temp=self.head
+        while(temp):
+            print(temp.data)
+            temp=temp.next
+list1=LinkedList()
+list1.head=Node(10)
+second=Node(20)
+third=Node(30)
+list1.head.next=second
+second.next=third
+list1.printList()
+
+        
